3ddd412bb3e7aa46f7c15b32629d99a406ffbc76
Darren Dooley <darrendooley8@gmail.com>
2026-02-15 21:42:38 +0000

    tabbing

--- lua/options.lua
+++ lua/options.lua
@@ -26,6 +26,12 @@ end)
 -- Enable break indent
 vim.o.breakindent = true
 
+-- Default indentation: 4 spaces (guess-indent.nvim overrides for existing files)
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+
 -- Save undo history
 vim.o.undofile = true
 
